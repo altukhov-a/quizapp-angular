a13ec58d2973bda9bc6dfeeb4bd6a307be92ada3
altukhov-a <a.altukhov85@gmail.com>
2015-07-22 00:20:31 +0300

    Add partial pages

---
+++ js/controllers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* Controllers */
+
+var quizControllers = angular.module('quizControllers', []);
+
+quizControllers.controller('QuizListCtrl', ['$scope',
+  function($scope) {
+  }
+]);
+
+quizControllers.controller('UserFormCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+	  
+  }
+]);
+
+quizControllers.controller('QuizResultCtrl', ['$scope',
+  function($scope) {
+  }
+]);
